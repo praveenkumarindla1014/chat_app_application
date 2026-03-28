7f0287038f4eb7b220ab390b3655f26de6fe5f0d
praveenkumarindla1014 <praveenkumarindla1014@gmali.com>
2026-03-28 23:02:42 +0530

    sign up was created

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,13 @@
+import jwt from 'jsonwebtoken';
+export const generateToken = (userId) => {
+  return jwt.sign({ userId }, process.env.JWT_SECERT, { expiresIn: '7d' });
+res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "strict",
+});
+
+ return token;
+};
+
