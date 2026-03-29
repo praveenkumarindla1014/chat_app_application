a17552a7e501ff9e457a37b2f9ab180d7bfcb920
praveenkumarindla1014 <praveenkumarindla1014@gmali.com>
2026-03-29 12:20:23 +0530

    errors find and changed final changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
-import copkieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.route.js";
 
@@ -15,12 +14,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(copkieParser());
 
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/messages",messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
